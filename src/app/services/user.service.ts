ae27fe90d30cf3f9ab01592a652e39054ae22e6e
Daniel Lewis <daniellewis777@gmail.com>
2019-04-06 02:03:14 +0100

    added user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { catchError, map } from 'rxjs/operators'
+import { of } from 'rxjs'
+import { environment } from '@environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  constructor(private http: HttpClient) {}
+
+  doesUserExist(username: string) {
+    return this.http.head(environment.httpPhp + '/user-exists/' + encodeURIComponent(username)).pipe(
+      map(() => 'yes'),
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 404) return of('no')
+        return of('error')
+      })
+    )
+  }
+}
