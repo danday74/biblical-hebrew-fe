d1191abd14ffe2cd93fb7d0ff1fed4be2eef2836
Daniel Lewis <daniellewis777@gmail.com>
2019-04-09 01:40:17 +0100

    updated prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // noinspection JSUnusedGlobalSymbols
 export const environment = {
   production: true,
-  httpPhp: 'http://localhost:4001',
+  httpPhp: 'http://localhost:4000/api',
   wsPhp: 'ws://localhost:4002'
 }
