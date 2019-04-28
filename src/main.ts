4e2dda2d6f2098b7fc113ddc2a0d4db55add0144
Daniel Lewis <daniellewis777@gmail.com>
2019-04-28 01:42:27 +0100

    recaptcha bugfix

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { enableProdMode } from '@angular/core'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from '@app/app.module'
 import { environment } from '@environments/environment'
+import * as Debug from 'debug'
+import { RecaptchaComponent } from 'ng-recaptcha'
+
+const debug = Debug('bh:main')
 
 if (environment.production) {
   enableProdMode()
@@ -9,3 +13,10 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err))
+
+RecaptchaComponent.prototype.ngOnDestroy = function() {
+  if (this.subscription) {
+    debug('RecaptchaComponent fix see https://github.com/DethAriel/ng-recaptcha/issues/123')
+    this.subscription.unsubscribe()
+  }
+}
