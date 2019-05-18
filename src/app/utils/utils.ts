1359ed327f3aac7d7ec004afdd00ccd24ac21c57
Daniel Lewis <daniellewis777@gmail.com>
2019-05-18 20:56:58 +0100

    linting

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -39,16 +39,16 @@ export const getBrowser = () => {
   return browser
 }
 
+export const hasHebrewCharsOnly = str => {
+  return str && RegExp('^[\u0590-\u05FF]+$').test(str)
+}
+
 export const getDir = str => {
   return hasHebrewCharsOnly(str) ? 'rtl' : 'ltr'
 }
 
 export const getRandomItemFromArray = (array: Array<any>) => {
   return array[Math.floor(Math.random() * array.length)]
-}
-
-export const hasHebrewCharsOnly = str => {
-  return str && RegExp('^[\u0590-\u05FF]+$').test(str)
 }
 
 export const asNumber = str => {
